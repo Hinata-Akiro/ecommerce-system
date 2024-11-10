aa4014edf19306d6abfc446a74e352f56fbb548a
VictorBarny <victor@myitura.com>
2024-11-10 15:36:49 +0100

    moved the mongodb to the app module

--- inventory-service/src/app.module.ts
+++ inventory-service/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InventoryModule } from './inventory/inventory.module';
 import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [InventoryModule, RabbitMQModule],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/inventory',
+    ),
+    InventoryModule,
+    RabbitMQModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- inventory-service/src/inventory/inventory.module.ts
+++ inventory-service/src/inventory/inventory.module.ts
@@ -9,9 +9,6 @@ import { RabbitMQModule } from 'src/rabbitmq/rabbitmq.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/inventory',
-    ),
     MongooseModule.forFeature([
       { name: Inventory.name, schema: InventorySchema },
     ]),
